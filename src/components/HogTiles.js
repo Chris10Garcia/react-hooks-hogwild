b4bceb624fd907231edff82c321fa12974cf950c
Christian Garcia <bones192nd@gmail.com>
2023-04-03 19:59:51 -0400

    fix HogCard component issue with file not working but function works

--- src/components/HogTiles.js
+++ src/components/HogTiles.js
@@ -1,40 +1,40 @@
 import React from "react"
-// import HogCard from "./HogCard"
+import HogCard from "./HogCard"
 
-function HogCard ({hog}) {
-    const {name, specialty, greased, weight, image} = hog
+// function HogCard ({hog}) {
+//     const {name, specialty, greased, weight, image} = hog
 
-    const award = hog["highest medal achieved"]
+//     const award = hog["highest medal achieved"]
 
-    function showHideCard(){
-        const divDescription = document.querySelector('#' + name + 'toHide')
-        divDescription.hidden = !divDescription.hidden
-        console.log(name)
-   }
+//     function showHideCard(){
+//         const divDescription = document.querySelector('#' + name + 'toHide')
+//         divDescription.hidden = !divDescription.hidden
+//         console.log(name)
+//    }
 
-return(
-    <div className="card" onClick={() =>showHideCard()}>
+// return(
+//     <div className="card" onClick={() =>showHideCard()}>
                 
-        <div className="image" style={{pointerEvents: "none"}}>
-            <img src = {image} alt={name} ></img>
-        </div>
+//         <div className="image" style={{pointerEvents: "none"}}>
+//             <img src = {image} alt={name} ></img>
+//         </div>
 
-        <div className="content" style={{pointerEvents: "none"}}>
-            <div className="header">
-                <h2 className="ui header">{name}</h2>
-            </div>
+//         <div className="content" style={{pointerEvents: "none"}}>
+//             <div className="header">
+//                 <h2 className="ui header">{name}</h2>
+//             </div>
 
-            <div className="description" hidden={true} id={name + "toHide" }>
-                <p>Specialty: {specialty}</p>
-                <p>Greased: {String(greased)}</p>
-                <p>Weight: {weight.toFixed(2)}</p>
-                <p>Hightest medal achieved: {award}</p>
-            </div>
-        </div>
+//             <div className="description" hidden={true} id={name + "toHide" }>
+//                 <p>Specialty: {specialty}</p>
+//                 <p>Greased: {String(greased)}</p>
+//                 <p>Weight: {weight.toFixed(2)}</p>
+//                 <p>Hightest medal achieved: {award}</p>
+//             </div>
+//         </div>
 
-    </div>
-    )
-}
+//     </div>
+//     )
+// }
 
 
 function HogTiles({hogs}){
